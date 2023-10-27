07afbeee84fef60b383ad59c8e99d8c68d62d613
Piotr Mazur <piotrjm02@gmail.com>
2023-10-28 01:05:09 +0200

    Add: Root controller

---
+++ cAPParel.API/Controllers/RootController.cs
@@ -0,0 +1,38 @@
+﻿using cAPParel.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace cAPParel.API.Controllers
+{
+    
+    [ApiController]
+    [Route("api")]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot()
+        {
+            var links = new List<LinkDto>();
+            links.Add(
+                new LinkDto(
+                href: Url.Link("GetRoot", new { }),
+                rel: "self",
+                method: "GET"));
+
+            links.Add(
+                new LinkDto(
+                href: Url.Link("GetCategories", new { }),
+                rel: "categories",
+                method: "GET"));
+
+            links.Add(
+                new LinkDto(
+                href: Url.Link("CreateCategory", new { }),
+                rel: "create_category",
+                method: "POST"));
+       
+            return Ok(links);
+        }
+       
+    }
+}
